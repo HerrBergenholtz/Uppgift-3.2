bbc9f252c4f3d9b4b8fdc434c3e026e1274f427b
HerrBergenholtz <zbdv8800@gmail.com>
2023-10-06 13:10:55 +0200

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ur gymnasiet? (svara \"j\" för ja och \"n\" för nej)");
+            string gymnasiet = Console.ReadLine().ToLower();
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (gymnasiet == "j" & ålder <= 22){
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
